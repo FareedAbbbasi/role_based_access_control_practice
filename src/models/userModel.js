421b3d66935c479b2308a59fe081280050b35e57
FareedAbbbasi <codingabbas0999@gmail.com>
2025-09-20 15:51:52 +0500

    make a model of the user

---
+++ src/models/userModel.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enums: ["admin", "manager", "user"],
+    }
+}, {timestamps:true})
+
+module.exports = mongoose.model("User", userSchema)
