fd9a2dd6cf22b6a44ddde9829492b48af42e9d0b
FareedAbbbasi <codingabbas0999@gmail.com>
2025-09-20 20:31:38 +0500

    add the authmiddleware and roleMiddleware

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,20 +1,20 @@
 const express = require("express")
+const verifyToken = require("../middlewares/authMiddleware")
+const authorizeRoles = require("../middlewares/roleMiddleware")
 const router = express.Router();
 
-
 // admin route
-router.get("/admin", (req, res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
     res.json({message: "welcome Admin"})
 })
 
 // manager route
-router.get("/manager", (req, res) => {
+router.get("/manager", verifyToken, authorizeRoles("admin", "manager"),  (req, res) => {
     res.json({message: "welcome manager"})
 })
 
-
 // user route
-router.get("/user", (req, res) => {
+router.get("/user", verifyToken, authorizeRoles("admin", "manager", "user"), (req, res) => {
     res.json({message: "welcome user"})
 })
 
